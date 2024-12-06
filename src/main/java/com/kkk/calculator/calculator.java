a2a9867552128117008993486545fbc68dce9e2d
mehdi <faddak.mehdi@gmail.com>
2024-12-06 11:15:02 +0100

    commit updated tests

--- src/main/java/com/kkk/calculator/calculator.java
+++ src/main/java/com/kkk/calculator/calculator.java
@@ -1,9 +1,9 @@
 package com.kkk.calculator;
 public class calculator {
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
-    public float  divide(float a,float b){
+    public static  float  divide(float a,float b){
         if(b!=0){
         return (a/b);}
         else{
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,19 +1,49 @@
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.kkk.calculator.calculator;
 
 public class CalculatorTest {
 
-    @Test
-    public void testAddition() {
-        calculator ct = new calculator();
-        int result = ct.add(2, 3);
-        assertEquals(5, result, "2 + 3 doit être égal à 5");
+    
+
+
+    private calculator ct;
+
+
+
+    
+    @BeforeEach
+    void setUp() {
+        ct = new calculator(); 
     }
+
+    @AfterEach
+    void tearDown() {
+        ct = null; 
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, 1, 1",
+        "1, 2, 3",
+        "-2, 2, 0",
+        "0, 0, 0",
+        "-1, -2, -3"
+    })
+    
+    public void testAddition(int opG,int opD,int expectedResult) {
+        int result = calculator.add(opG, opD);
+        assertEquals(expectedResult, result, "Addition incorrecte pour les opérandes : " + opG + ", " + opD);
+    }
+
+
     @Test
     public void testDivision() {
-        calculator ct = new calculator();
         float result = ct.divide(2, 3);
         assertEquals(2.0f / 3.0f, result, 0.0001, "2 / 3 doit être égal à 2/3");
          result = ct.divide(2, 0);
